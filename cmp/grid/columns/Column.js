xh/hoist-react

commit 01d6dc3a9a5a502b791a22e2011e4b9d72ea2028

fixes #2068 :  Dragging grid column with element based header display… (#2085)
* fixes #2068 :  Dragging grid column with element based header displays [object Object] in draggable

* add #2068 fix to v37 bugFix list

* Apply Lee's suggestions from CR.

cmp/grid/columns/Column.js
@@ -8,7 +8,7 @@ import {div} from '@xh/hoist/cmp/layout';
 import {XH} from '@xh/hoist/core';
 import {genDisplayName} from '@xh/hoist/data';
 import {throwIf, warnIf, withDefault} from '@xh/hoist/utils/js';
-import {castArray, clone, find, get, isArray, isFinite, isFunction, isNil, isNumber, isEmpty} from 'lodash';
+import {castArray, clone, find, get, isArray, isFinite, isFunction, isNil, isNumber, isEmpty, isString} from 'lodash';
 import {Component} from 'react';
 import {GridSorter} from '../impl/GridSorter';
 import {ExportFormat} from './ExportFormat';
@@ -316,9 +316,9 @@ export class Column {
                 field,
                 colId: this.colId,
                 headerValueGetter: (agParams) => {
-                    return agParams.location === 'header' ?
-                        isFunction(headerName) ? headerName({column: this, gridModel, agParams}) : headerName :
-                        this.chooserName;
+                    return agParams.location === 'header' && isString(headerName) ?
+                        headerName :
+                        this.displayName;
                 },
                 headerClass: getAgHeaderClassFn(this),
                 headerTooltip: this.headerTooltip,

cmp/grid/impl/ColumnHeader.js
@@ -68,7 +68,7 @@ export const columnHeader = hoistCmp.factory({
         const {xhColumn, gridModel} = impl,
             {isDesktop} = XH;
 
-        const headerName = isFunction(xhColumn?.headerName) ?
+        const headerElem = isFunction(xhColumn?.headerName) ?
             xhColumn.headerName({column: xhColumn, gridModel}) :
             props.displayName;
 
@@ -77,7 +77,8 @@ export const columnHeader = hoistCmp.factory({
         if (isDesktop && isUndefined(xhColumn?.headerTooltip)) {
             onMouseEnter = ({target: el}) => {
                 if (el.offsetWidth < el.scrollWidth) {
-                    el.setAttribute('title', headerName);
+                    const title = isString(headerElem) ? headerElem : props.displayName;
+                    el.setAttribute('title', title);
                 } else {
                     el.removeAttribute('title');
                 }
@@ -94,7 +95,7 @@ export const columnHeader = hoistCmp.factory({
             onTouchEnd:     !isDesktop ? impl.onTouchEnd : null,
 
             items: [
-                span({onMouseEnter, item: headerName}),
+                span({onMouseEnter, item: headerElem}),
                 sortIcon(),
                 menuIcon()
             ]

commit a584eaa83e39e7f7246bf595043c4dcfa1288be7

Fix regression in headerValueGetter caused by PR #2085

cmp/grid/columns/Column.js
@@ -8,7 +8,7 @@ import {div} from '@xh/hoist/cmp/layout';
 import {XH} from '@xh/hoist/core';
 import {genDisplayName} from '@xh/hoist/data';
 import {throwIf, warnIf, withDefault} from '@xh/hoist/utils/js';
-import {castArray, clone, find, get, isArray, isFinite, isFunction, isNil, isNumber, isEmpty, isString} from 'lodash';
+import {castArray, clone, find, get, isArray, isFinite, isFunction, isNil, isNumber, isEmpty, isNull} from 'lodash';
 import {Component} from 'react';
 import {GridSorter} from '../impl/GridSorter';
 import {ExportFormat} from './ExportFormat';
@@ -310,15 +310,16 @@ export class Column {
      * Produce a Column definition appropriate for AG Grid.
      */
     getAgSpec() {
-        const {gridModel, field, headerName} = this,
+        const {gridModel, field, headerName, displayName} = this,
             me = this,
             ret = {
                 field,
                 colId: this.colId,
                 headerValueGetter: (agParams) => {
-                    return agParams.location === 'header' && isString(headerName) ?
-                        headerName :
-                        this.displayName;
+                    if (agParams.location !== 'header') return displayName;
+                    if (isNull(headerName)) return null;
+
+                    return isFunction(headerName) ? displayName : headerName;
                 },
                 headerClass: getAgHeaderClassFn(this),
                 headerTooltip: this.headerTooltip,